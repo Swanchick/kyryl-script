15e0f5ec864e850a35f5f3bd10411a40b8ff082f
Swanchick <61909965+Swanchick@users.noreply.github.com>
2025-04-13 01:12:47 +0300

    Added example folder, and did some upgrades

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,41 @@ mod parser;
 mod interpreter;
 
 use std::io;
+use std::env;
 
 use interpreter::{interpreter::Interpreter, value::Value};
 use lexer::lexer::Lexer;
 use parser::parser::Parser;
 
-
-fn main() {
-    let mut lexer = Lexer::load("test.kys").unwrap();
-    lexer.lexer().unwrap();
-
-    let mut parser = Parser::new(lexer.get_tokens().clone());
-    let functions = parser.parse_functions().unwrap();
-
-    let mut interpreter = Interpreter::new();
-    interpreter.register_rust_function("print", |args: Vec<Value>| {
-        let arg = args.get(0);
-
-        if let Some(arg) = arg {
+fn register_standart_library(interpreter: &mut Interpreter) {
+    interpreter.register_rust_function("println", |args: Vec<Value>| {
+        for arg in args {
             match arg {
-                Value::Integer(var) => println!("{}", var),
-                Value::Float(var) => println!("{}", var),
-                Value::Boolean(var) => println!("{}", var),
-                Value::String(var) => println!("{}", var),
-                Value::Void => println!("void :)"),
+                Value::Integer(var) => print!("{}", var),
+                Value::Float(var) => print!("{}", var),
+                Value::Boolean(var) => print!("{}", var),
+                Value::String(var) => print!("{}", var),
+                Value::Void => print!("void :)"),
+                _ => return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    format!("Unsupported value to print: {}", arg.get_data_type())
+                ))
+            }
+        }
+
+        println!("");
+        
+        Ok(Value::Void)
+    });
+
+    interpreter.register_rust_function("print", |args: Vec<Value>| {
+        for arg in args {
+            match arg {
+                Value::Integer(var) => print!("{}", var),
+                Value::Float(var) => print!("{}", var),
+                Value::Boolean(var) => print!("{}", var),
+                Value::String(var) => print!("{}", var),
+                Value::Void => print!("void :)"),
                 _ => return Err(io::Error::new(
                     io::ErrorKind::InvalidData,
                     format!("Unsupported value to print: {}", arg.get_data_type())
@@ -36,28 +47,36 @@ fn main() {
         
         Ok(Value::Void)
     });
+}
 
-    interpreter.register_rust_function("unprint", |args: Vec<Value>| {
-        let arg = args.get(0);
+fn run_script(script_path: &str) -> io::Result<()> {
+    let mut lexer = Lexer::load(script_path)?;
+    lexer.lexer()?;
 
-        if let Some(arg) = arg {
-            match arg {
-                Value::Integer(var) => println!("Delete {}", var),
-                Value::Float(var) => println!("Delete {}", var),
-                Value::Boolean(var) => println!("Delete {}", var),
-                Value::String(var) => println!("Delete {}", var),
-                Value::Void => println!("void :)"),
-                _ => return Err(io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    format!("Unsupported value to print: {}", arg.get_data_type())
-                ))
-            }
-        }
+    let mut parser = Parser::new(lexer.get_tokens().clone());
+    let functions = parser.parse_functions()?;
 
-        Ok(Value::Void)
-    });
+    let mut interpreter = Interpreter::new();
+    register_standart_library(&mut interpreter);
 
-    interpreter.interpret_program(functions).unwrap();
+    interpreter.interpret_program(functions)?;
+
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let script_path = args.get(1);
+
+    if let Some(script_path) = script_path {
+        let result = run_script(script_path);
+
+        if let Err(e) = result {
+            println!("========== KyrylScript Error!!!");
+            println!("{}", e);
+            println!("===============================");
+        } 
+    }
 }
 
 
