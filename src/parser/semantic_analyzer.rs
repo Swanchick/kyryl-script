85480f38b531a3facbf540f1f55ea167e0019eb2
Swanchick <61909965+Swanchick@users.noreply.github.com>
2025-05-22 21:43:37 +0300

    Added addition/division/difference/multiplication and etc result datatype in semantic analyzer

--- src/parser/semantic_analyzer.rs
+++ src/parser/semantic_analyzer.rs
@@ -1,12 +1,11 @@
 use std::io;
 use std::collections::HashMap;
 
-use crate::parser::expression;
 use crate::parser::operator::Operator;
 
 use super::data_type::DataType;
 use super::expression::Expression;
-use super::operator;
+
 
 pub struct SemanticAnalyzer {
     variables: HashMap<String, DataType>
@@ -22,59 +21,68 @@ impl SemanticAnalyzer {
     // * addition/division/difference/multiplication and etc result datatype
 
     fn plus(&self, left: DataType, right: DataType) -> io::Result<DataType> {
-        todo!()
+        match (left, right) {
+            (DataType::Int, DataType::Int) => Ok(DataType::Int),
+            (DataType::Float, DataType::Int) => Ok(DataType::Float),
+            (DataType::Int, DataType::Float) => Ok(DataType::Float),
+            (DataType::Float, DataType::Float) => Ok(DataType::Float),
+            (DataType::String, DataType::String) => Ok(DataType::String),
+            _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Arithmetic type error!"))
+        }
+    }
+
+    fn arithmetic(&self, left: DataType, right: DataType) -> io::Result<DataType> {
+        match (left, right) {
+            (DataType::Int, DataType::Int) => Ok(DataType::Int),
+            (DataType::Float, DataType::Int) => Ok(DataType::Float),
+            (DataType::Int, DataType::Float) => Ok(DataType::Float),
+            (DataType::Float, DataType::Float) => Ok(DataType::Float),
+            _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Arithmetic type error!"))
+        }
+    }
+
+    fn boolean(&self, left: DataType, right: DataType) -> io::Result<DataType> {
+        match (left, right) {
+            (DataType::Bool, DataType::Bool) => Ok(DataType::Bool),
+            _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Logic type error!"))
+        }
+    }
+
+    fn comparison(&self, left: DataType, right: DataType) -> io::Result<DataType> {
+        match (left, right) {
+            (DataType::Int, DataType::Int) 
+            | (DataType::Float, DataType::Int)
+            | (DataType::Int, DataType::Float)
+            | (DataType::Float, DataType::Float)
+            | (DataType::String, DataType::String) => Ok(DataType::Bool),
+            _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Arithmetic type error!"))
+        }
+    }
+
+    fn logic(&self, left: DataType, right: DataType) -> io::Result<DataType> {
+        match (left, right) {
+            (DataType::Int, DataType::Int) 
+            | (DataType::Float, DataType::Int)
+            | (DataType::Int, DataType::Float)
+            | (DataType::Float, DataType::Float) => Ok(DataType::Bool),
+            _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Arithmetic type error!"))
+        }
     }
     
     fn binary_operation(&self, operator: Operator, left: DataType, right: DataType) -> io::Result<DataType> {
         match operator {
-            Operator::Plus => {
-                self.plus(left, right)
-            }
-
-            Operator::Minus => {
-                todo!()
-            }
-
-            Operator::Multiply => {
-                todo!()
-            }
-
-            Operator::Divide => {
-                todo!()
-            }
-
-            Operator::And => {
-                todo!()
-            }
-
-            Operator::Or => {
-                todo!()
-            }
-
-            Operator::EqualEqual => {
-                todo!()
-            }
-
-            Operator::NotEqual => {
-                todo!()
-            }
-
-            Operator::GreaterEqual => {
-                todo!()
-            }
-
-            Operator::Greater => {
-                todo!()
-            }
-
-            Operator::LessEqual => {
-                todo!()
-            }
-
-            Operator::Less => {
-                todo!()
-            }
-
+            Operator::Plus => self.plus(left, right),
+            Operator::Minus => self.arithmetic(left, right),
+            Operator::Multiply => self.arithmetic(left, right),
+            Operator::Divide => self.arithmetic(left, right),
+            Operator::And => self.boolean(left, right),
+            Operator::Or => self.boolean(left, right),
+            Operator::EqualEqual => self.comparison(left, right),
+            Operator::NotEqual => self.comparison(left, right),
+            Operator::GreaterEqual => self.logic(left, right),
+            Operator::Greater => self.logic(left, right),
+            Operator::LessEqual => self.logic(left, right),
+            Operator::Less => self.logic(left, right),
             _ => unreachable!()
         }
     }
