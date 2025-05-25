537527ba196f23e3553d45ea944c8c8f3e62cca7
Swanchick <61909965+Swanchick@users.noreply.github.com>
2025-05-25 03:12:01 +0300

    Removed unnecessary function in NativeRegistry

--- src/native_registry/native_registry.rs
+++ src/native_registry/native_registry.rs
@@ -15,11 +15,7 @@ impl NativeRegistry {
     pub fn register_function(&mut self, name: &str, rust_function: RustFunction) {
         self.rust_functions.insert(name.to_string(), rust_function);
     }
-
-    pub fn get_rust_function(&self, name: &str) -> Option<&RustFunction> {
-        self.rust_functions.get(name)
-    }
-
+    
     pub fn get(&self) -> &HashMap<String, RustFunction> {
         &self.rust_functions
     }
