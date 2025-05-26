4fe8941830a9f1bca9bbf51186c68807f1859444
Swanchick <61909965+Swanchick@users.noreply.github.com>
2025-05-26 14:39:09 +0300

    Update enviroment.rs

--- src/interpreter/enviroment.rs
+++ src/interpreter/enviroment.rs
@@ -1,7 +1,7 @@
 use std::io;
 use std::collections::HashMap;
 use std::rc::Rc;
-use std::cell::{Ref, RefCell};
+use std::cell::RefCell;
 
 use super::value::Value;
 use super::variable_slot::VariableSlot;
