393e5377cb17c536338b4139b2c6de96cb9e5874
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-07-07 23:32:40 +0300

    - Refactor `NativeRegistry` to use `thread_local!` for instance management, removing `Once` and `unsafe` usage - Update project to Rust 2024 edition in `Cargo.toml` - Add `.idea` folder to `.gitignore`

--- src/native_registry/native_registry.rs
+++ src/native_registry/native_registry.rs
@@ -1,18 +1,16 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::collections::HashMap;
-use std::sync::Once;
 
 
 use crate::interpreter::enviroment::Environment;
-use crate::interpreter::interpreter::Interpreter;
-use crate::interpreter::value::Value;
 use super::native_buffer::NativeBuffer;
 
 use super::native_types::NativeTypes;
 
-static INIT: Once = Once::new();
-static mut NATIVE_REGISTRY: Option<Rc<RefCell<NativeRegistry>>> = None;
+thread_local! {
+    static NATIVE_REGISTRY: Rc<RefCell<NativeRegistry>> = NativeRegistry::new();
+}
 
 pub struct NativeRegistry {
     pub global: Option<Rc<RefCell<Environment>>>,
@@ -22,13 +20,7 @@ pub struct NativeRegistry {
 
 impl NativeRegistry {
     pub fn get() -> Rc<RefCell<NativeRegistry>> {
-        unsafe {
-            INIT.call_once(|| {
-                NATIVE_REGISTRY = Some(NativeRegistry::new());
-            });
-
-            NATIVE_REGISTRY.as_ref().unwrap().clone()
-        }
+        NATIVE_REGISTRY.with(|registry| registry.clone())
     }
 
     pub fn new() -> Rc<RefCell<NativeRegistry>> {
